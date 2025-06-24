587c0c4c7c75396a57e4bfabd8f81af3069b14dc
Mark Finerty <finertymark@gmail.com>
2025-06-24 12:08:38 -0400

    Update: Add flyer for 'A Taste of Magic' event.

--- src/components/MyFlyer.jsx
+++ src/components/MyFlyer.jsx
@@ -2,7 +2,7 @@
 // eslint-disable-next-line react/prop-types
 function MyFlyer({imgSrc, altText}) {
   return (
-    <img className="w-[30rem] bg-miss-ivy-green text-center font-bold md:text-7xl text-miss-ivy-gold border-double border-4 border-miss-ivy-gold text-3xl p-2"
+    <img className="w-[30rem] lg:w-[45rem] bg-miss-ivy-green text-center font-bold md:text-7xl text-miss-ivy-gold border-double border-4 border-miss-ivy-gold text-3xl p-2"
       src={imgSrc}
       alt={altText}>
     </img>
--- src/sections/EventSection.jsx
+++ src/sections/EventSection.jsx
@@ -6,12 +6,15 @@ function EventSection() {
     <>
       <MyBanner title="Events" />
       <section id="events" className="scroll-mt-56 sm:scroll-mt-96">
-        <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          <MyFlyer imgSrc="./thursday.jpeg" altText="thursday-flyer" />
-          <MyFlyer imgSrc="./friday.jpeg" altText="friday-flyer" />
-          <MyFlyer imgSrc="./saturday.jpeg" altText="saturday-flyer" />
-          <MyFlyer imgSrc="./sunday.jpeg" altText="sunday-flyer" />
-        </div>
+        {/* <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"> */}
+        <MyFlyer imgSrc="./magic-man.jpeg" altText="christian-mescia" />
+        <a
+          href="https://www.eventbrite.ca/e/a-taste-of-magic-tickets-1413125830399?aff=oddtdtcreator-"
+          target="_blank"
+          className="flex justify-self-center justify-center rounded-lg bg-miss-ivy-green px-4 py-2 mt-5 text-white hover:bg-green-800 border-4 border-miss-ivy-gold border-double w-96"
+        >
+          Get Tickets
+        </a>
       </section>
     </>
   );
